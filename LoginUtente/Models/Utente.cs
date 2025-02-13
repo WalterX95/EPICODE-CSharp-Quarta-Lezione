1c30fd70ada02106f05f7e7ff80d8af99f315e61
WalterX95 <walter_1995@hotmail.it>
2025-02-13 16:44:20 +0100

    Add files via upload

---
+++ LoginUtente/Models/Utente.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualBasic;
+using System.IO;
+
+namespace LoginUtente.Models
+{
+    internal static class Utente
+    {
+        private static string Username {get; set;} = string.Empty;
+        private static string Password { get; set; } = string.Empty;
+        private static bool IsLogged { get; set;} = false;
+        private static DateTime Ora { get; set; }
+        private static DateOnly DataLogin { get; set; }
+        private static string[] DataBaseUtenti { get; set; } = { Username, Password };
+
+        private static string[] DataConnessione { get; set;} = { };
+        public static string ControlUserAndLogin() {  
+            foreach (var user in DataBaseUtenti)
+            {
+                if(Username != user || Password != user)
+                {
+                    Console.WriteLine("Username Non Corretto, Riprova per favore");
+                }
+                else
+                {
+
+                    Console.WriteLine("Username e Password Corretto");
+                    IsLogged = true;
+                    Ora = DateTime.Now;
+                    DataLogin = new DateOnly();
+                    DataConnessione.Append(Ora.ToString());
+                    DataConnessione.Append(DataLogin.ToString()); 
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Accesso Effettuato");
+                    Console.ForegroundColor = ConsoleColor.White;
+                    if (IsLogged)
+                    {
+                        _ = File.Create("LogFile.txt");
+                        _ = File.OpenText("LogFile.txt");
+                        _ = File.AppendText(DataLogin.ToString()+"\n");
+                        _ = File.AppendText(Ora.ToString() + "\n");
+                    }
+                }
+            }
+            return Username; 
+        } 
+
+        public static void ListaAccessi()
+        {
+            foreach (var accessi in DataConnessione)
+            {
+                Console.WriteLine($"Ora e Data : {accessi.ToString()}");
+            }
+        }
+    }
+}
---
+++ LoginUtente/Program.cs
@@ -0,0 +1,23 @@
+﻿using LoginUtente.Models;
+
+var IlMioUtente = Utente.ControlUserAndLogin();
+var SecondoUtente = Utente.ControlUserAndLogin();
+
+var Operation = true;
+
+while(Operation)
+{
+
+    Console.WriteLine("==================== LOGIN MENU =======================");
+    Console.WriteLine("Scegli Operazione da Compiere");
+    Console.WriteLine("1) Login");
+    Console.WriteLine("2) Logout");
+    Console.WriteLine("3) Verifica ora e data di Login");
+    Console.WriteLine("4) Lista degli accessi");
+    Console.WriteLine("5) Esci");
+    Console.WriteLine("============================== =======================");
+
+}
+
+
+
